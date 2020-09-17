274b9cd0d52d3751a5c8d803b4c3bf60253e3326
Rocio Batres <jonasgirl21@gmail.com>
2020-09-17 05:24:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Dog 
+  def name=(give_name)
+    @give_a_name = give_name  
+  end 
+  def name 
+    @give_a_name
+  end 
+  def bark
+    puts "woof!"
+  end   
+end   
+
